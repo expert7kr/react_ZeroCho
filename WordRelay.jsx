c4eabd38b759e69f21ef8c6b9b7357972658a366
expert7kr <expert7kr@naver.com>
2019-09-22 14:15:05 +0900

    React 기본 강좌 2-5. 웹팩으로 빌드하기 : 4. 최종적으로 화면에 'Hello, webpack' 나오게 하기

--- WordRelay.jsx
+++ WordRelay.jsx
@@ -1,15 +1,16 @@
 const React = require('react');
 const { Component } = React; // 아래 주석을 줄일 수 있음
+// 위 2줄은 필요한 모듈을 불러오는 것
 
 // class WordRelay extends React.Component {
     class WordRelay extends Component {
     state = {
-
+        text: 'Hello, webpack',
     }
     render() {
-
+        return <h1>{this.state.text}</h1>;
     }
 }
 
-module.exports = WordRelay; // 파일을 분리시킬 때는 이것과 위 2줄을 추가해야 함
-
+module.exports = WordRelay; // 밖에서 사용할 수 있게 해줌
+// 파일을 분리시킬 때는 이것과 위 2줄을 추가해야 함
