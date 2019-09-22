74dce227e9986bbe220e218cd5a2c917fe260b5d
expert7kr <expert7kr@naver.com>
2019-09-22 09:11:57 +0900

    React 기본 강좌 2-4. 모듈 시스템과 웹팩 설정: 1. 이렇게 jsx파일에 클래스를 적고 적용해도 됨 > 컴포넌트 관리가 어려움

--- client.jsx
+++ client.jsx
@@ -1,3 +1,13 @@
 const React = require('react');
 const ReactDom = require('react-dom');
 
+class WordRelay extends React.Component {
+    state = {
+
+    }
+    render() {
+
+    }
+}
+
+ReactDom.render(<WordRelay />, document.querySelector('#root'));
